bee4f2d184d587cf7d508b44805c166ba7690bfa
Souhail Mardi <souhailmardi77@gmail.com>
2025-10-01 07:48:43 -0700

    Resolve merge conflict in app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,7 +8,3 @@ def home():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-=======
-for i in range(1,10):
-	print ("bonjour tout le monde")
->>>>>>> d1894c5e221f39c30dd1d778ac1c1ec10c0b8777
